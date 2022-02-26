a0483f21fb288471d9cf3901e07cf2cb05a37af6
chaitanya-Uike <chaitanyaguike@gmail.com>
2022-02-26 13:49:31 +0530

    initial commit

---
+++ public/js/room.js
@@ -0,0 +1,117 @@
+const socket = io('/')
+
+socket.emit('join-room', roomId)
+
+const canvasContainer = document.querySelector("#canvasContainer")
+const canvasBounds = canvasContainer.getBoundingClientRect()
+
+let paintObject = { path: [] }
+
+let color = 'black'
+let strokeWidth = 5
+
+let canvasWidth = canvasBounds.width - 2
+let canvasHeight = canvasBounds.height - 2
+
+function setup() {
+    let canvas = createCanvas(canvasWidth, canvasHeight);
+    canvas.parent('canvasContainer')
+
+    background('white')
+}
+
+function mouseDragged() {
+    stroke(color)
+    strokeWeight(strokeWidth)
+
+    line(mouseX, mouseY, pmouseX, pmouseY)
+
+    paintObject.path.push([mouseX, mouseY])
+}
+
+function mouseReleased() {
+    paintObject.color = color
+    paintObject.strokeWidth = strokeWidth
+
+    socket.emit("send-paint-path", paintObject)
+
+    // clear the paint object
+    paintObject.path = []
+}
+
+function paintPath(paintObject) {
+    const path = paintObject.path
+
+    stroke(paintObject.color)
+    strokeWeight(paintObject.strokeWidth)
+
+    for (let i = 0; i < path.length - 1; i++) {
+        line(path[i][0], path[i][1], path[i + 1][0], path[i + 1][1])
+    }
+}
+
+
+// socket events
+socket.on("paint", paintObject => {
+    paintPath(paintObject)
+})
+
+socket.on("send-state", user => {
+    const canvas = document.getElementById("defaultCanvas0")
+
+    socket.emit("send-canvas-state", user, canvas.toDataURL())
+})
+
+socket.on("get-canvas-state", data => {
+    loadImage(data, img => {
+        image(img, 0, 0, canvasWidth, canvasHeight);
+    });
+})
+
+socket.on('clear-canvas', () => {
+    background('white')
+})
+
+
+// settings
+const colorPicker = document.querySelector("#stroke-color")
+const eraser = document.querySelector('#eraser')
+const strokeRange = document.querySelector("#stroke-Range")
+const clearBtn = document.querySelector("#clear-canvas")
+
+
+colorPicker.addEventListener('change', e => {
+    color = e.target.value
+})
+
+colorPicker.addEventListener('click', e => {
+    color = e.target.value
+})
+
+eraser.addEventListener('click', () => {
+    color = 'white'
+})
+
+strokeRange.addEventListener("change", e => {
+    strokeWidth = e.target.value
+
+    // drawingCursor.style.width = e.target.value + "px"
+    // drawingCursor.style.height = e.target.value + "px"
+
+    // cursorBounds = drawingCursor.getBoundingClientRect()
+})
+
+clearBtn.addEventListener("click", e => {
+    background('white')
+    socket.emit("trigger-clear-canvas", roomId)
+})
+
+// document.querySelector("body").addEventListener("mousemove", e => {
+//     if (!Array.from(e.target.classList).includes("canvas-element")) {
+//         isDrawing = false
+//         drawingCursor.style.display = "none"
+//     }
+//     else {
+//         drawingCursor.style.display = ""
+//     }
+// })
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const { v4: uuid } = require('uuid')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    const roomId = uuid()
+    res.redirect(roomId)
+})
+
+app.get('/:roomId', (req, res) => {
+    const roomId = req.params.roomId
+    res.render('room', { roomId })
+})
+
+io.on("connection", socket => {
+    socket.on("join-room", roomId => {
+        let clientSet = io.sockets.adapter.rooms.get(roomId)
+        let host
+        if (clientSet)
+            host = [...clientSet][0]
+
+        socket.join(roomId)
+
+        if (host)
+            socket.to(host).emit('send-state', socket.id)
+
+        socket.on("send-canvas-state", (user, data) => {
+            socket.to(user).emit("get-canvas-state", data)
+        })
+
+        socket.on("send-paint-path", paintObject => {
+            socket.to(roomId).emit("paint", paintObject)
+        })
+
+        socket.on("start-drawing", ({ x, y }) => {
+            socket.to(roomId).emit("move-to", { x, y })
+        })
+
+        socket.on("drawing", ({ x, y }) => {
+            socket.to(roomId).emit("line-to", { x, y })
+        })
+
+        socket.on("trigger-clear-canvas", roomId => {
+            socket.to(roomId).emit('clear-canvas')
+        })
+    })
+})
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, console.log("Server listening on PORT 3000..."))
